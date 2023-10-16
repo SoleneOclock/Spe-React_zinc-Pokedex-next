fd223a024832c0fbf2af035de65b936a08586399
SoleneOclock <solene@oclock.io>
2023-10-16 11:36:02 +0200

    on fetch les données coté serveur, on construit ainsi la page avant de l'envoyer au client.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,31 +10,27 @@
 // c'est un peu dommage de mettre un useclient sur la page sinon y'a plus de ssr
 // donc on va plutot faire des sous composants client et garder nos pages en server
 
-'use client';
-
 import Counter from '@/components/Counter';
 import PokemonCard from '@/components/PokemonCard';
-import { useEffect } from 'react';
 
-export default function Home() {
+const getData = async () => {
+  const response = await fetch('https://api-pokemon-fr.vercel.app/api/v1/gen/9');
+  const pokemonList = await response.json();
+  console.log(pokemonList);
+  // dans le tableau pokemonList on a nos objets pokemon
+
+  return pokemonList;
+};
+
+// grace à next on peut avoir des composants qui sont des fonction asynchrones
+// on va pouvoir attendre le fetch des données avant de faire le rendu du composant
+export default async function Home() {
   // recuperer une liste de pokemon d'une API
   // dans le JSX pour chaque pokemon de la list on va afficher un sous composant PokemonCard
   console.log('rendu du composant Home !!! ');
 
-  const getData = async () => {
-    const response = await fetch('https://api-pokemon-fr.vercel.app/api/v1/gen/9');
-    const pokemonList = await response.json();
-    console.log(pokemonList);
-    // dans le tableau pokemonList on a nos objets pokemon
-  };
-
-  useEffect(() => {
-    // je veux recup les pokemon une seule fois après le premier rendu uniquement
-    // ça serait quand meme plus pratique de laisser cette page en server component
-    // d'aller fetch les data depuis le serveur
-    // de construire la page et de l'envoyer au client
-    getData();
-  }, []);
+  const pokemonList = await getData();
+  console.log(pokemonList);
 
   return (
     <main>
@@ -44,9 +40,13 @@ export default function Home() {
         </h1>
         <Counter />
 
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
+        <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-2 p-2'>
+          {
+            pokemonList.map((pokemon) => (
+              <PokemonCard key={pokemon.pokedexId} pokemon={pokemon} />
+            ))
+          }
+        </div>
 
       </div>
     </main>
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,9 +1,28 @@
 // sans ajout de 'use client';, ce composant sera rendu coté server
 
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
   return (
-    <div>
-      une carte
+    <div className="bg-cyan-900 rounded-lg shadow-lg p-4 flex flex-col w-full hover:bg-cyan-800">
+      <img
+        src={pokemon.sprites.shiny || pokemon.sprites.regular}
+        alt={pokemon.name.fr}
+      />
+      <div className="py-2 text-center">
+        <h3 className="font-bold text-cyan-400">
+          {`#${pokemon.pokedexId} - ${pokemon.name.fr}`}
+        </h3>
+        <h4 className="text-cyan-600 text-center">
+          {pokemon.name.en}
+        </h4>
+      </div>
+      <p className="flex flex-wrap gap-2 justify-center">
+        {pokemon.types?.map((type) => (
+          <span key={type.name} className="flex items-center gap-2 text-sm text-slate-300">
+            {type.name}
+            <img src={type.image} alt={type.name} className="w-6" />
+          </span>
+        ))}
+      </p>
     </div>
   );
 }
